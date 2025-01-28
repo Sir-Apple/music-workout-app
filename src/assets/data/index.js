04a2cede6759a83d87daa660cd120683e2d4e3f4
Anhh <tranquangtuananh2801@gmail.com>
2025-01-28 20:55:38 +0700

    update

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,66 +1,66 @@
 const Data = [
   {
     id: 1,
-    title: 'Nothing Stopping Me',
-    artist: 'TrackTribe',
+    title: 'Nothing Stopping Me Remix',
+    artist: 'Silver Smoke Remix',
     thumbnail: require('../images/img01.jpg'),
-    src: require('../musics/nothing-stopping-me.mp3')
+    src: require('../musics/Nothing Stopping Me Remix 2x.mp3')
   },
   {
     id: 2,
-    title: 'Coast - Anno Domini Beats',
-    artist: 'Anno Domini Beats',
+    title: 'RAVE',
+    artist: 'Dxrk',
     thumbnail: require('../images/img02.jpg'),
-    src: require('../musics/Coast - Anno Domini Beats.mp3')
+    src: require('../musics/Dxrk   RAVE Official Audio.mp3')
   },
   {
     id: 3,
-    title: 'Here it Comes - TrackTribe',
-    artist: 'TrackTribe',
+    title: 'BRODYAGA FUNK PHONK',
+    artist: 'Eternxlkz',
     thumbnail: require('../images/img03.jpg'),
-    src: require('../musics/Here it Comes - TrackTribe.mp3')
+    src: require('../musics/BRODYAGA FUNK PHONK.mp3')
   },
   {
     id: 4,
-    title: 'It Was a Time - TrackTribe',
-    artist: 'TrackTribe',
+    title: 'Go',
+    artist: 'Neffex',
     thumbnail: require('../images/img04.jpg'),
-    src: require('../musics/It Was a Time - TrackTribe.mp3')
+    src: require('../musics/Go.mp3')
   },
   {
     id: 5,
-    title: 'Mulholland - King Canyon',
-    artist: 'King Canyon',
+    title: 'Valhalla Calling',
+    artist: 'Miracle Of Sound ft. Peyton Parrish',
     thumbnail: require('../images/img05.jpg'),
-    src: require('../musics/Mulholland - King Canyon.mp3')
+    src: require('../musics/Valhalla Calling.mp3')
   },
   {
     id: 6,
-    title: 'No Mercy - TrackTribe',
-    artist: 'TrackTribe',
+    title: 'Slow Motion Deadwood',
+    artist: ' Epic Dark Rock Action',
     thumbnail: require('../images/img06.jpg'),
-    src: require('../musics/No Mercy - TrackTribe.mp3')
+    src: require('../musics/Slow Motion Deadwood.mp3')
   },
   {
     id: 7,
-    title: 'Put It - TrackTribe',
-    artist: 'TrackTribe',
+    title: 'Mikoto',
+    artist: 'Senzo',
     thumbnail: require('../images/img07.jpg'),
-    src: require('../musics/Put It - TrackTribe.mp3')
+    src: require('../musics/Mikoto.mp3')
   },
   {
     id: 8,
-    title: 'Read All Over - Nathan Moore',
-    artist: 'Nathan Moore',
+    title: 'Phonk In The End',
+    artist: 'Goldkid $',
     thumbnail: require('../images/img08.jpg'),
-    src: require('../musics/Read All Over - Nathan Moore.mp3')
+    src: require('../musics/Phonk In The End.mp3')
   },
   {
     id: 9,
-    title: 'Silver Waves - TrackTribe',
-    artist: 'TrackTribe',
+    title: 'SLAY',
+    artist: 'Eternxlkz',
     thumbnail: require('../images/img09.jpg'),
-    src: require('../musics/Silver Waves - TrackTribe.mp3')
+    src: require('../musics/Eternxlkz  SLAY Official Audio.mp3')
   }
 ]
 
